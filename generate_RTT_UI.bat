37384e8427a6dcda5273953e4a2927a57890fa49
Michal Kindl <kindl@rtt-zittau.de>
2023-02-06 11:50:46 +0100

    fix: paths in batch files, batch descriptions

--- generate_RTT_UI.bat
+++ generate_RTT_UI.bat
@@ -1,7 +1,8 @@
 :: run from project root directory
-:: parameters are [sources dir] [output dir]
+:: modify paths to fit desired project location
+:: parameters are [sources dir] [output dir] [showMethParams true/false] [showMethParamTypes true/false]
 dotnet run -- c:\Michal\Projekty\RTT_UI c:\Michal\Projekty\RTT_UI\Docs\uml true false
 
-:: generate png
+:: generate diagrams as png
 java -jar plantuml.jar "c:\Michal\Projekty\RTT_UI\Docs\uml\*.plantuml" "c:\Michal\Projekty\RTT_UI\Docs\uml"
 
--- generate_RTT_Utils.bat
+++ generate_RTT_Utils.bat
@@ -1,3 +1,7 @@
-# run from project root directory
-# parameters are [sources dir] [output dir]
+:: run from project root directory
+:: modify paths to fit desired project location
+:: parameters are [sources dir] [output dir] [showMethParams true/false] [showMethParamTypes true/false]
 dotnet run -- c:\Michal\Projekty\RTT_Utils\RTT_Utils c:\Michal\Projekty\RTT_Utils\RTT_Utils\Documentation\Docs\uml\classes  true true
+
+:: generate diagrams as png
+java -jar plantuml.jar "c:\Michal\Projekty\RTT_Utils\RTT_Utils\Documentation\Docs\uml\classes\*.plantuml" "c:\Michal\Projekty\RTT_Utils\RTT_Utils\Documentation\Docs\uml\classes"
